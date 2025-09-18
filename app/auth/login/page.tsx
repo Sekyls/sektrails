a55498b1cd375e1510af336ae74e0133efe1bdbb
Dennis Sekyi Opoku <denipsyp@gmail.com>
2025-09-18 06:35:04 +0000

    chore

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,6 @@
-import dynamic from "next/dynamic";
+"use client";
+import FirebaseAuthUI from "@/components/firebase/firebase-auth-ui";
 import React from "react";
-
-const FirebaseAuthUI = dynamic(
-  () => import("@/components/firebase/firebase-auth-ui"),
-  { ssr: false }
-);
 
 export default function AuthenticationPage() {
   return (
