3e3cdd50510887d2ed350d8fb428096519c8c891
Dennis Sekyi Opoku <denipsyp@gmail.com>
2025-09-21 03:01:47 +0000

    feat: next background video with mux

--- lib/constants.ts
+++ lib/constants.ts
@@ -12,22 +12,25 @@ import {
   IconView360,
   IconWorldStar,
 } from "@tabler/icons-react";
+import wednesday from "@/videos/wednesday.mp4";
+import wuthering from "@/videos/wuthering.mp4";
+import twot from "@/videos/twot.mp4";
 
 export const TRAILERS = [
   {
-    path: "trailers/wednesday.mp4",
+    path: wednesday,
     title: "Wednesday Season II",
     description:
       "While attending Nevermore Academy, Wednesday Addams attempts to master her emerging psychic ability, thwart a killing spree and solve the mystery that embroiled her parents 25 years ago.",
   },
   {
-    path: "trailers/wuthering.mp4",
+    path: wuthering,
     title: "Wuthering Heights",
     description:
       "Tragedy strikes when Heathcliff falls in love with Catherine Earnshaw, a woman from a wealthy family in 18th-century England.",
   },
   {
-    path: "trailers/twot.mp4",
+    path: twot,
     title: "The Wheel Of Time",
     description:
       "Moiraine, a member of a magical organization, takes five young people on a journey, believing that one of them might be the reincarnation of the Dragon, a powerful individual prophesied to save the world or destroy it.",
